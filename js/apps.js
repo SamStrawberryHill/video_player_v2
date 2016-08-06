5bf3f4a0855b0697168e7b9fc0b5f8e3a83d0d4b
Samantha Hill <samhill5643@gmail.com>
2016-08-06 16:25:06 -0400

    Fullscreen Fix

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,14 @@
+
+
+
+
 $('#play, #pause').click(function() {
 	playPauseVideo();
 });
+$('#button-fullscreen').click(function(){
+	handleFullscreen();
+});
+
 
 // Function for play video and change button to pause
 function playPauseVideo() {
@@ -14,4 +22,34 @@ function playPauseVideo() {
 			$('#pause').css('display', 'none');
 			$('#play').css('display', 'inline-block');
    }
+ }
+ // Full Screen
+ var fullScreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
+ if (!fullScreenEnabled) {
+    fullscreen.style.display = 'none';
+ }
+function handleFullscreen() {
+		var fullscreen = document.getElementById('button-fullscreen').addEventListener('click',function() {
+	handleFullscreen();
+		var isFullScreen = function() {
+   return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+}
+	if (isFullScreen()) {
+      if (document.exitFullscreen) document.exitFullscreen();
+      else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+      else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+      else if (document.msExitFullscreen) document.msExitFullscreen();
+      setFullscreenData(false);
+   }
+   else {
+      if (videoContainer.requestFullscreen) videoContainer.requestFullscreen();
+      else if (videoContainer.mozRequestFullScreen) videoContainer.mozRequestFullScreen();
+      else if (videoContainer.webkitRequestFullScreen) videoContainer.webkitRequestFullScreen();
+      else if (videoContainer.msRequestFullscreen) videoContainer.msRequestFullscreen();
+      setFullscreenData(true);
+   }
+	 var isFullScreen = function() {
+   return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+		}
+});
 }
