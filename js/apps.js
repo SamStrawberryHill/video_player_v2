5bf631e890e44928da26ebff0d14ca0c3715f5d8
Samantha Hill <samhill5643@gmail.com>
2016-08-12 18:19:08 -0400

    Fix Progress Bar

--- js/apps.js
+++ js/apps.js
@@ -7,6 +7,15 @@ $('#play, #pause').click(function() {
 $('#button-fullscreen').click(function(){
 	handleFullscreen();
 });
+volumeOn.addEventListener('click', function() {
+	muteVideo();
+});
+volumeOff.addEventListener('click', function() {
+	muteVideo();
+});
+$('#progressBar').click(function(){
+	progressBar();
+});
 
 // Function for play video and change button to pause
 function playPauseVideo() {
@@ -22,14 +31,23 @@ function playPauseVideo() {
 	}
 }
 //Volume On and Mute
-//Volume On show volume button
-//Mute show mute button
+function  muteVideo() {
+   video.muted = !video.muted;
+	 if (video.muted) {
+		$('#volumeOn').css('display', 'none');
+		$('#volumeOff').css('display', 'inline-block');
+	}
+	else {
+		$('#volumeOff').css('display', 'none');
+		$('#volumeOn').css('display', 'inline-block');
+	}
+}
 
 // Full Screen
-	var fullScreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
-	if (!fullScreenEnabled) {
-		fullscreen.style.display = 'none';
-	}
+var fullScreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
+if (!fullScreenEnabled) {
+	fullscreen.style.display = 'none';
+}
 function handleFullscreen() {
 	var isFullScreen = function() {
 		return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
@@ -50,6 +68,13 @@ function handleFullscreen() {
 //Implement the playback progress control.
 //A user should be able to click anywhere on the playback bar to jump to that part of the video.
 //As the video plays the playback bar should fill in.
+var video = document.getElementById('video');
+var pBar = document.getElementById('progressBar');
+video.addEventListener('timeupdate', function() {
+  var percent = Math.floor((100 / video.duration) * video.currentTime);
+  pBar.value = percent;
+  pBar.getElementsByTagName('span')[0].innerHTML = percent;
+}, false);
 //As the video plays the current time should be displayed and updated e.g. 0:10 / 11:34.
 //Use Javascript or CSS to hide and show the video player button on mouse hover states. Only the progress bar should remain.
 //As the media playback time changes, sentences in the transcript should highlight.
